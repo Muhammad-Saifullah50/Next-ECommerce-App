4c830ec913363b607c58785c69a679ec66fea71b
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-04-22 21:17:09 +0500

    added carousel

--- src/pages/homePage/Carousel/Carousel.js
+++ src/pages/homePage/Carousel/Carousel.js
@@ -1,7 +1,11 @@
+import styles from '@/styles/carousel.module.css'
 const Carousel = () => {
 
     return (<>
-      
+    <div className={styles.imgslider}>
+
+    </div>
     </>)
-}
+    }
+       
 export default Carousel
