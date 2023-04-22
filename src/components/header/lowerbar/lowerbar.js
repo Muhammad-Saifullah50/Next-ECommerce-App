a73d0a3836cbeeebc1c75d073388b8ba48db9086
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-04-22 12:26:21 +0500

    completed lowerbar

--- src/components/header/lowerbar/lowerbar.js
+++ src/components/header/lowerbar/lowerbar.js
@@ -12,19 +12,22 @@ const lowerbar = () => {
         </Head>
         <div className={styles.lowerbar}>
             <div className={styles.logobox}>
-                <Image className={styles.logo} src="/images/logo.png" width={100}
+                <Image className={styles.logo} src="/images/logo1.png" width={100}
                     height={80}>
 
                 </Image>
             </div>
             <div className={styles.searchbox}>
-                <input type="text" className={styles.search} placeholder="I am looking for ..." />
-                <Image className={styles.searchicon} src="/images/search.png" width={15} height={15}>
+                <input type="text" className={styles.search} placeholder="      I am looking for ..." />
 
+                <Image className={styles.searchicon} src="/images/search.png" width={15} height={15}>
                 </Image>
             </div>
             <div className={styles.cartbox}>
-                klklklklk
+               <div className={styles.carttext}>&nbsp;Shopping Cart</div> 
+                <Image src="/images/cart1.png" width={25} height={23}>
+
+                </Image>
             </div>
         </div>
     </>)
