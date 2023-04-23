ab7ef7b799a28c29c8a3015e902035aefb485c47
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-04-23 09:56:03 +0500

    completed banner

--- src/pages/homePage/Carousel/Carousel.js
+++ src/pages/homePage/Carousel/Carousel.js
@@ -2,10 +2,13 @@ import styles from '@/styles/carousel.module.css'
 const Carousel = () => {
 
     return (<>
-    <div className={styles.imgslider}>
-
-    </div>
+        <div className={styles.imgslider}>
+            <div className={styles.carouseltext}>
+                <h2>Professional Power Tools</h2>
+                <p>Over the years, we have created professional power tools and equipments that complemet your construction and building process.</p>
+            </div>
+        </div>
     </>)
-    }
-       
+}
+
 export default Carousel
