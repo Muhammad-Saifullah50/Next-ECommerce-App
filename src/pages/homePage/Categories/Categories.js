f832e37b89107ee8143c430207e65a81ab497d8f
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-04-24 14:51:00 +0500

    added cateories on hme page

--- src/pages/homePage/Categories/Categories.js
+++ src/pages/homePage/Categories/Categories.js
@@ -4,30 +4,51 @@ import Image from 'next/image'
 const Categories = () => {
 
     return (<>
-
-        <div className={styles.categories}>
+        <div className={styles.heading}>
             <h2>Select a category to get started</h2>
+        </div>
+        <div className={styles.categories}>
+
             <div className={styles.cardgrid}>
                 <div className={styles.cards}>
                     <Image src="/images/category1.png" width={144} height={144}>
 
                     </Image>
+                    <p>Heavy Duty Saws</p>
+
+
                 </div>
+
                 <div className={styles.cards}>
                     <Image src="/images/category2.png" width={144} height={144}>
 
-                    </Image>            </div>
+                    </Image>
+                    <p>Rotary Compressor</p></div>
                 <div className={styles.cards}>
-                    <Image src="/images/category3.png" width={144} height={144}>
-
-                    </Image>            </div>
+                    <Image src="/images/toolbox.png" width={144} height={144}>
+                    </Image>
+                    <p>Multipurpose Tool Box</p>
+                </div>
                 <div className={styles.cards}>
                     <Image src="/images/category4.png" width={144} height={144}>
 
                     </Image>
+                    <p>Heayv Duty Stapler</p>
+                </div>
+
+                <div className={styles.cards}>
+                    <Image src="/images/category3.png" width={144} height={144}>
+
+                    </Image>
+                    <p>Heavy Duty Polisher</p>
+
+                </div>
+                <div className={styles.cards}>
+                    <iframe width="192" height="192" src="https://www.youtube.com/embed/DMQ0-ENXxZg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
+
     </>)
 }
 
