6a5e3b0ba04a2861f6b8c0795738b36124d8f025
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-05-02 08:00:25 +0500

    added banner in categories page

---
+++ src/pages/categoriesPage/Banner/Banner.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/banner.module.css'
+const Banner = () => {
+
+    return (<>
+        <div className={styles.banner}>
+            <h2>Categories</h2>
+        </div>
+    </>)
+}
+
+export default Banner
---
+++ src/pages/categoriesPage/CategoriesPage.js
@@ -0,0 +1,11 @@
+import Header from "@/components/header/Header"
+import Banner from "./Banner/Banner"
+const CategoriesPage = () => {
+
+    return(<>
+    <Header />
+    <Banner />
+
+    </>)
+}
+export default CategoriesPage
