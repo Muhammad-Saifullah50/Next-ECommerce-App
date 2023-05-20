792267106527686a4cefc4ee2e4911d7f45656e6
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-05-20 20:35:35 +0500

    added loader, styling remains

--- src/pages/Loader.js
+++ src/pages/Loader.js
@@ -1,12 +1,27 @@
 import styles from '@/styles/loader.module.css'
+import { ThreeCircles } from 'react-loader-spinner'
+
+
 import Image from 'next/image'
 const Loader = () => {
 
     return (<>
         <div className={styles.loader}>
-            <Image src={'/images/logo1.png'} height={250} width={500} alt='logo'> 
+            <Image src='/images/dewaltlogo.png' height={250} width={500} alt='logo'>
 
             </Image>
+            <ThreeCircles
+                height="100"
+                width="100"
+                color="#4fa94d"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="three-circles-rotating"
+                outerCircleColor=""
+                innerCircleColor=""
+                middleCircleColor=""
+            />
         </div>
     </>
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,39 @@
 import '@/styles/globals.css'
+import Loader from './Loader'
+
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [loading, setLoading] = useState(false);
+  const router = useRouter()
+
+  useEffect(() => {
+
+    const handleRouteChange = () => {
+      setLoading(true)
+      return;
+    };
+
+    const handleRouteComplete = () => {
+      setLoading(false)
+      return;
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange)
+    router.events.on('routeChangeComplete', handleRouteComplete)// If the component is unmounted, unsubscribe
+
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [])
+  return (<>
+
+    {loading ? (<Loader />) : (
+      <Component {...pageProps} />
+    )
+    }
+  </>)
 }
