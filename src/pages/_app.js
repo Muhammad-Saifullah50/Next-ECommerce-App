690937eec2f02d0932fcabed5dd2b3f0b5235b4f
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-05-20 20:39:35 +0500

    correscted initial loading error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const router = useRouter()
 
   useEffect(() => {
