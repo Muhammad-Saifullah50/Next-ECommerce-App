544121fe0a1c7cdaa140d12bae4e90e7a262b15b
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-05-21 08:04:13 +0500

    corrected loader

--- src/pages/Loader.js
+++ src/pages/Loader.js
@@ -7,21 +7,25 @@ const Loader = () => {
 
     return (<>
         <div className={styles.loader}>
-            <Image src='/images/dewaltlogo.png' height={250} width={500} alt='logo'>
+            <div className={styles.logo}>
+                <Image src='/images/dewaltlogo.png' height={250} width={500} alt='logo'>
 
-            </Image>
-            <ThreeCircles
-                height="100"
-                width="100"
-                color="#4fa94d"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-                ariaLabel="three-circles-rotating"
-                outerCircleColor=""
-                innerCircleColor=""
-                middleCircleColor=""
-            />
+                </Image>
+            </div>
+            <div className={styles.circles}>
+                <ThreeCircles
+                    height="90"
+                    width="90"
+                    color="#4fa94d"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                    ariaLabel="three-circles-rotating"
+                    outerCircleColor="#e3b212"
+                    innerCircleColor="black"
+                    middleCircleColor="#e3b212"
+                />
+            </div>
         </div>
     </>
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,25 +5,45 @@ import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
-  const router = useRouter()
+  const router = useRouter();
+
+  const initialLoading = () => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+
+    return timer; // Return the timeout reference
+  };
+  
+
+  const timerRef = initialLoading();
 
   useEffect(() => {
     const handleRouteChangeStart = () => {
-      setLoading(true)
-    }
+      setLoading(true);
+    };
 
     const handleRouteChangeComplete = () => {
-      setLoading(false)
-    }
+      setLoading(false);
+    };
 
-    router.events.on('routeChangeStart', handleRouteChangeStart)
-    router.events.on('routeChangeComplete', handleRouteChangeComplete)
+    const handleRouteChangeError = () => {
+      setLoading(false);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    router.events.on('routeChangeError', handleRouteChangeError);
 
     return () => {
-      router.events.off('routeChangeStart', handleRouteChangeStart)
-      router.events.off('routeChangeComplete', handleRouteChangeComplete)
-    }
-  }, [])
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      router.events.off('routeChangeError', handleRouteChangeError);
+
+      clearTimeout(timerRef);
+
+    };
+  }, []);
 
   return (
     <>
@@ -33,5 +53,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       )}
     </>
-  )
+  );
 }
