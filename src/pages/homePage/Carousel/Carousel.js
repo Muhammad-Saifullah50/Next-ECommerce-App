a09e5d85049be9e1ae9e4254e6895a6dcbf2c71d
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-05-25 14:30:15 +0500

    corrected carousel

--- src/pages/homePage/Carousel/Carousel.js
+++ src/pages/homePage/Carousel/Carousel.js
@@ -1,19 +1,24 @@
-import styles from '@/styles/carousel.module.css'
+import styles from '@/styles/carousel.module.css';
+import Head from 'next/head';
+
 const Carousel = () => {
+  return (
+    <>
+      <Head>
+        <link rel="preload" href="/images/slide1.png" as="image" />
+        <link rel="preload" href="/images/slide2.png" as="image" />
+        <link rel="preload" href="/images/slide3.png" as="image" />
+        <link rel="preload" href="/images/slide4.png" as="image" />
+      </Head>
 
-    return (<>
-        <div className={styles.imgslider}>
-            <div className={styles.carouseltext}>
-                <h2>Professional Power Tools</h2>
-                <p>Over the years, we have created professional power tools and equipments that complement your construction and building process.</p>
-                
-            </div>
+      <div className={styles.imgslider}>
+        <div className={styles.carouseltext}>
+          <h2>Professional Power Tools</h2>
+          <p>Over the years, we have created professional power tools and equipment that complement your construction and building process.</p>
         </div>
+      </div>
+    </>
+  );
+};
 
-    </>)
-}
-
-export default Carousel
-
-
- 
+export default Carousel;
