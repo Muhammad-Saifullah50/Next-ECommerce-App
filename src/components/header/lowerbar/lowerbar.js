212138b9935872ccc35f89e9f0f6aba33c3aba4e
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-06-01 16:18:21 +0500

    making lowerrbar responsive

--- src/components/header/lowerbar/lowerbar.js
+++ src/components/header/lowerbar/lowerbar.js
@@ -1,8 +1,3 @@
-
-import styles from '@/styles/lowerbar.module.css'
-// import '@/styles/globals.css'
-
-
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -10,30 +5,33 @@ const lowerbar = () => {
 
     return (<>
 
-        <div className={styles.lowerbar}>
-            <div className={styles.logobox}>
-                <Image className={styles.logo} src="/images/logo1.png" width={100}
+        <div className="lowerbar flex justify-between items-center">
+            <div className="logobox w-1/2">
+                <Image className="logo" src="/images/logo1.png" width={100}
                     height={80} alt='logo'>
 
                 </Image>
             </div>
-            <div className={styles.searchbox}>
-                <input type="text" className={styles.search} placeholder="      I am looking for ..." />
 
-                <Image className={styles.searchicon} src="/images/search.png" width={15} height={15} alt='search icon'>
+            <div className="search hidden md:flex">
+                <input type="text" className="search" placeholder="      I am looking for ..." />
+
+                <Image className="searchicon" src="/images/search.png" width={15} height={15} alt='search icon'>
                 </Image>
             </div>
-            <Link href='/cartPage/CartPage'>
-                <button className={styles.cartbox}>
-                    <div className={styles.carttext}>&nbsp;Shopping Cart</div>
-                    <Image src="/images/cart1.png" width={25} height={23} alt='cart'>
 
-                    </Image>
-
-                </button>
-            </Link>
-
+            <div className="cartbox flex justify-between">
+                <Link href='/cartPage/CartPage'>
+                    <div className="carttext">&nbsp;Shopping Cart</div>
+                    <div className="cartimg">
+                        <Image src="/images/cart1.png" width={25} height={23} alt='cart'>
+                        </Image>
+                    </div>
+                </Link>
+            </div>
         </div>
+
+
     </>)
 }
 
--- src/components/header/topbar/topbar.js
+++ src/components/header/topbar/topbar.js
@@ -1,28 +1,28 @@
-// import styles from '@/styles/topbar.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 const topbar = () => {
 
     return (<>
 
-        <div className="topbar flex w-[100vh] h-[5vh] bg-black text-white">
-            <div className="phno hidden sm:block w-[20vw] text-center">
-                <Image className="phone invert mt-[6px]" src="/images/phone.png" width={15} height={15} alt="phone">
+        <div className="topbar flex w-auto h-[5vh] bg-black text-white justify-around ">
+            <div className="phno hidden sm:flex  items-center text-center">
+                <Image className="phone invert mt-[6px] ml-2 mb-2" src="/images/phone.png" width={15} height={15} alt="phone">
                 </Image>
                 &nbsp;+1234567890
-
             </div>
-            <div className="timing hidden md:block w-[40vw] text-center">
-                <Image className="clock mt-[6px]" src="/images/clock.jpg" width={15} height={15} alt='clock'>
+
+            <div className="timing hidden md:flex w-fit md:w-auto justify-between items-center  text-center">
+                <Image className="clock" src="/images/clock.jpg" width={15} height={15} alt='clock'>
 
                 </Image>
                 &nbsp; Mon - Sun 8:00 am - 5:00 pm
             </div>
-            <div className="sidebox w-[39vw mt-1]">
-                <ul className="topnav md:absolute md:right-1 flex justify-between list-none">
-                    <li className='hidden md:block border-[1px] border-gray-400 pr-5 hover:underline'>ENG</li>
-                    <Link className='no-underline text-white' href='/aboutPage/AboutPage'>   <li>About Us</li> </Link>
-                    <li className='border-[1px] border-gray-400 pr-5 hover:underline'>Customer Service</li>
+            <div className="sidebox  flex w-auto justify-center items-center mt-1">
+                <ul className="topnav  flex justify-between list-none">
+                    <li className='hidden md:block pr-5 hover:underline'>ENG</li>
+                    <Link className='no-underline text-white' href='/aboutPage/AboutPage'>
+                        <li className='pr-5 hover:underline'>About Us</li> </Link>
+                    <li className='pr-5 hover:underline'>Customer Service</li>
                     <li className='pr-5 hover:underline '>Login</li>
                 </ul>
             </div>
